4d47ecfc049c2118ef575b4b9acf4118b1eafd75
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-05-16 03:52:38 +0300

    completed home work 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Lesson1 from './lessons/lesson1/lesson1';
-// import './lessons/lesson2/lesson2';
+//import Lesson1 from './lessons/lesson1/lesson1';
+import './lessons/lesson2/lesson2';
 // import Lesson3 from './lessons/lesson3/Lesson3';
 // import Lesson4 from './lessons/lesson4/Lesson4';
 // import './lessons/lesson5/lesson5';
@@ -10,9 +10,10 @@ import Lesson1 from './lessons/lesson1/lesson1';
 // import './lessons/lesson8/lesson8';
 
 function App() {
+
     return (
         <div className="container">
-            <Lesson1 />
+            {/*<Lesson1 />*/}
             {/*<Lesson3 />*/}
             {/*<Lesson4 />*/}
         </div>
--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -23,6 +23,12 @@ console.log('lesson 2');
 
 // Task 01
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
+/*function sum (a: number) {
+    return function (b: number) {
+        return a + b
+    }
+}
+console.log(sum(3)(6) === 9)*/
 
 // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
@@ -32,6 +38,23 @@ console.log('lesson 2');
 // const counter2 = makeCounter();
 // counter2(); // 1
 // counter(); // 3
+/*
+function  makeCounter () {
+    let  count= 0;
+    return function () {
+        return ++count;
+    }
+}
+const counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+const counter2 = makeCounter();
+console.log(counter2());
+console.log(counter2());
+console.log(counter());
+*/
+
 
 // Task 03
 // Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
@@ -40,6 +63,21 @@ console.log('lesson 2');
 // decrease: -1
 // reset: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
+
+/*function  makeCounter (count: number) {
+    debugger
+    let counter= count
+      return function () {
+        return {
+            increase: ++count,
+            decrease: --counter,
+            reset: count=0,
+            set: counter
+      }
+    }
+}
+const counter = makeCounter(2);
+console.log(counter())*/
 
 // Task 04*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
@@ -52,6 +90,36 @@ console.log('lesson 2');
 // 6) superSum(3)(2,5)(3,9) //10
 
 // P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
+/*
+function  superSum (n: number) {
+    if (n<=0) {return 1}
+    if (n === 1) {return (n: number)=> n}
+
+    let _args: Array<number>= []
+    function helper (...args: Array<number>) {
+        _args = [..._args, ...args]
+        if(_args.length >=n){
+        _args.length =n
+            return _args.reduce((acc, number)=> acc+number)
+        } else {return  helper}
+    }
+    return  helper
+}
+
+superSum(0)
+//@ts-ignore
+superSum(3)(2)(5)(3)
+//@ts-ignore
+superSum(3)(2)(5,3)
+//@ts-ignore
+superSum(3)(2,5,3)
+//@ts-ignore
+superSum(3)(2,5)(3)
+//@ts-ignore
+superSum(3)(2,5)(3,9)
+
+*/
+
 
 // Task 05
 // решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
